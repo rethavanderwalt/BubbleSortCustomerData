5c68aa69648e7df8f8533458ba15902af271124f
Retha <91030418@mail.computerpower.ac.nz>
2018-09-26 08:58:15 +1200

    2nd draft with bubble sort

--- src/bubblesortcustomerdata/BubbleSortCustomerData.java
+++ src/bubblesortcustomerdata/BubbleSortCustomerData.java
@@ -11,15 +11,38 @@ package bubblesortcustomerdata;
  */
 public class BubbleSortCustomerData {
 
-    /**
-     * @param args the command line arguments
-     */
+    // use bubble sort
+        void doBubbleSort(int arr[])
+        {
+            int n = arr.length;
+            for (int i = 0; i < n-1; i++)
+                for (int j = 0; j < n-i-1; j++)
+                    if (arr[j] > arr[j+1])
+                    {
+                        // swap temp and arr[i]
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+        }
+        
+    // Prints the array 
+        void printArray(int arr[])
+        {
+            int n = arr.length;
+            for (int i=0; i<n; ++i)
+                System.out.print(arr[i] + " ");
+            System.out.println();
+            System.out.println();
+        }
+        
+    
     public static void main(String[] args) {
         // welcome message
         
         // display unsorted collection
         
-        // use bubble sort
+       
         
         // sort by customer rating (ascending)
             // then by customer name (decending)
