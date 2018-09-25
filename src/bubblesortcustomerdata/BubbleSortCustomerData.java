3f3d14d06ab80b0656d159b54834bbce055d8cb7
Retha <91030418@mail.computerpower.ac.nz>
2018-09-26 08:52:18 +1200

    1st draft

---
+++ src/bubblesortcustomerdata/BubbleSortCustomerData.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bubblesortcustomerdata;
+
+/**
+ *
+ * @author 91030418
+ */
+public class BubbleSortCustomerData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // welcome message
+        
+        // display unsorted collection
+        
+        // use bubble sort
+        
+        // sort by customer rating (ascending)
+            // then by customer name (decending)
+        
+        // display sorted data
+            // terminate program
+            
+            
+        BubbleSortCustomerData ob = new BubbleSortCustomerData();
+        CustomerData arr[] = {
+            new CustomerData("Tama", 2, "Auckland"),
+            new CustomerData("Amelia", 5, "Taranaki"),
+            new CustomerData("Kaos", 3, "Hamilton"),
+            new CustomerData("Karl", 4, "Papatoetoe"),
+            new CustomerData("Carlos", 5, "Glenfield"),
+            new CustomerData("Alicia", 2, "Whangarai"),
+            new CustomerData("Zion", 2, "Wellingotn"),
+            new CustomerData("Sara", 3, "Auckland"),
+            new CustomerData("Bob", 4, "Papakura"),
+            new CustomerData("Wiremu", 5, "Auckland"),
+        };
+    }
+    
+}
