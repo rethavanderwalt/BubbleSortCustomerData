0a9bd062ba3463c7d7b48cb5b6b634260202d730
Retha <91030418@mail.computerpower.ac.nz>
2018-09-26 11:35:45 +1200

    6th draft with bubble sort for customer rating and customer name (Names are now in descending order)

--- src/bubblesortcustomerdata/BubbleSortCustomerData.java
+++ src/bubblesortcustomerdata/BubbleSortCustomerData.java
@@ -17,7 +17,7 @@ public class BubbleSortCustomerData {
             int n = arr.length;
             for (int i = 0; i < n; i++)
                 for (int j = 1; j < (n-i); j++)
-                    if (arr[j-1].getName().compareTo(arr[j].getName()) > 0)
+                    if (arr[j-1].getName().compareTo(arr[j].getName()) < 0)
                     {
                         // swap temp and arr[i]
                         CustomerData temp = arr[j-1];
