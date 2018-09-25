475f6d8140d179963b91c5af3a4d6a34ab778919
Retha <91030418@mail.computerpower.ac.nz>
2018-09-26 08:54:18 +1200

    Customer data file

---
+++ src/bubblesortcustomerdata/CustomerData.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bubblesortcustomerdata;
+
+/**
+ *
+ * @author 91030418
+ */
+public class CustomerData {
+    String name;
+    int customerRating;
+    String address;
+     
+    public CustomerData (String name, int customerRating, String address) {
+        this.name = name;
+        this.customerRating = customerRating;
+        this.address = address;
+    }
+    
+    public String toString(){
+        return "Customer Name: " + name + 
+                "\nCustomer rating: " + customerRating + 
+                "\nCustomer address: " + address + "\n" + "\n";
+    }
+}
